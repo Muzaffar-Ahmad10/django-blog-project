b9feb71f5568fd997bcfa640b1a3e0f7ceff32e4
Muzaffar Tahir <muzaffarahmad709@gmail.com>
2024-11-09 10:22:17 +0000

    create post model

--- env.py
+++ env.py
@@ -3,3 +3,4 @@ import os
 os.environ.setdefault(
     "DATABASE_URL", "postgres://ufze1nm0aee:wqe9qB6LQXci@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/yodel_dot_path_288154")
 os.environ.setdefault("SECRET_KEY", "django-insecure--0k=f@2shp7%*hl+&*$2-3*7ixi54_0v9ksxrm)%24j%k=2s40")
+
