e7f1a91a86dfa57062b154fa4bd87f04cff0885a
Muzaffar Tahir <muzaffarahmad709@gmail.com>
2024-11-13 12:30:46 +0000

    Deployment with static files

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "8000-muzaffarahmad10-math-4uf0nik4z4n.ws.codeinstitute-ide.net",
