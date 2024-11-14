96a44e093631a15f78163cdd51678153af48701b
Muzaffar Tahir <muzaffarahmad709@gmail.com>
2024-11-14 21:27:53 +0000

    Restricting access

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,6 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
 ALLOWED_HOSTS = [
     "8000-muzaffarahmad10-math-4uf0nik4z4n.ws.codeinstitute-ide.net",
     ".herokuapp.com"
