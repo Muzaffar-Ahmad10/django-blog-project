715d22e80b7b483d41c3ddfc2bc8ba2c72dcf807
Muzaffar Tahir <muzaffarahmad709@gmail.com>
2024-11-16 13:39:38 +0000

    enable serving of image files

--- env.py
+++ env.py
@@ -4,3 +4,5 @@ os.environ.setdefault(
     "DATABASE_URL", "postgres://ufze1nm0aee:wqe9qB6LQXci@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/shock_glow_think_222347")
 os.environ.setdefault("SECRET_KEY", "django-insecure--0k=f@2shp7%*hl+&*$2-3*7ixi54_0v9ksxrm)%24j%k=2s40")
 
+os.environ.setdefault(
+    "CLOUDINARY_URL", "cloudinary://<your_api_key>:<your_api_secret>@djr6ul2t9")
